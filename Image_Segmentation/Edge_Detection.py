020619b17c781a4622d8c67773414dfa57a27820
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-26 15:37:59 +0530

    .

---
+++ Image_Segmentation/Edge_Detection.py
@@ -0,0 +1,8 @@
+"""
+Edge-based Segmentation:
+Definition: Edge-based segmentation detects boundaries or edges in an image to separate different objects.
+Real-world Explanation: Locating boundaries between objects based on abrupt changes in intensity or color.
+Working: Edges are detected by identifying significant changes in pixel intensity.
+Use Case: Useful in robotics for object manipulation, where precise object boundaries are critical.
+Influence on CV: Enables the identification and extraction of object boundaries for further analysis
+"""
