a64c998681cc0e0804ecbbaa1759a24acfa816b4
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-27 14:30:31 +0530

    .

---
+++ Object_detection/YouOnlyLookOnce(YOLO).py
@@ -0,0 +1,19 @@
+"""
+You Only Look Once (YOLO):
+Definition:
+YOLO is an object detection algorithm that divides an image into a grid and predicts bounding boxes and class probabilities directly at each grid cell.
+
+Real-world Explanation:
+YOLO processes the entire image in one forward pass, providing real-time object detection.
+
+Working:
+
+Grid Division: The image is divided into a grid.
+Predictions: For each grid cell, YOLO predicts bounding boxes and class probabilities.
+Use Case:
+YOLO is widely used in scenarios where real-time object detection is crucial, such as surveillance or robotics.
+
+Influence on CV:
+YOLO revolutionized real-time object detection by providing a good balance between speed and accuracy
+
+"""
