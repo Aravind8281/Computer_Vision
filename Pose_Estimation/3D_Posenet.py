c471118455d4d1ac8b17f42dee91c76a411e1f82
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-29 20:39:00 +0530

    .

---
+++ Pose_Estimation/3D_Posenet.py
@@ -0,0 +1,6 @@
+"""
+    Definition: 3D pose estimation involves determining the positions of key body joints in a 3D space from a 2D image or video.
+    Real-world Explanation: Extending beyond 2D, 3D pose estimation reconstructs the spatial positions of joints in three dimensions, providing a more comprehensive understanding of the subject's pose.
+    Working: Often requires additional information, such as camera calibration parameters, to convert 2D joint positions into 3D coordinates.
+    Use Case: Augmented reality, robotics, biomechanics, sports performance analysis.
+"""
