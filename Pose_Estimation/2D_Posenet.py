e7741b64251e264220f38bc611374abb8efd62ab
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-29 20:38:04 +0530

    Create 2D_Posenet.py

---
+++ Pose_Estimation/2D_Posenet.py
@@ -0,0 +1,6 @@
+"""
+    Definition: 2D pose estimation involves determining the positions of key body joints in a 2D space from a 2D image or video.
+    Real-world Explanation: Given an image or video, 2D pose estimation identifies the locations of joints such as shoulders, elbows, and knees, representing the body's posture in a 2D plane.
+    Working: Typically involves using algorithms or neural networks to detect and localize key body joints based on features present in the 2D image.
+    Use Case: Human-computer interaction, gesture recognition, action recognition in videos, sports analysis.
+"""
