1bae1921217965a3b9dfe36ec040679d5dde9c8d
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-31 21:18:01 +0530

    .

---
+++ Image_Registration/intensityBased_registration.py
@@ -0,0 +1,11 @@
+"""
+Definition: Intensity-based registration involves aligning images by optimizing the similarity of their intensity distributions.
+
+Real World Explanation: Aligning two medical images with different contrasts or lighting conditions.
+
+Working: It involves finding a transformation that maximizes the similarity metric (correlation, mutual information) between pixel intensities.
+
+Use Case: Image fusion for improved visibility in aerial or medical imaging.
+
+Influence on CV: Important for various tasks like medical image analysis where images may have different acquisition conditions
+"""
